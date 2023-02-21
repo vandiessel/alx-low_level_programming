ff6d6918c798690f64290b432f4bd802fd914788
vandiessel <nicholasdarkwah233@gmail.com>
2023-02-20 21:49:03 -0800

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* Positive anything is better than negative nothing*/
-   Return: Always 0 (Success)
+/*
+* main - Positive or negative
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -18,6 +20,7 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	else
+	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
