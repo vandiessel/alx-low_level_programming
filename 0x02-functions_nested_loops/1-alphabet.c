9e175bbcf147e1c94aebe9e6a87fbb6f32eb6899
vandiessel <nicholasdarkwah233@gmail.com>
2023-03-02 15:15:20 -0800

    0x02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+* Print the alphabet in lowercase
+* Return: Always 0
+*/
+void print_alphabet(void);
+{
+	int a;
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+         	_putchar('\n');
+
