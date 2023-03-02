50df611f6755d89f5fbabb6d97ae962ca05296c7
vandiessel <nicholasdarkwah233@gmail.com>
2023-03-02 15:13:56 -0800

    0x01

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Print Alphabet
+ *Return: Always 0
+*/
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
